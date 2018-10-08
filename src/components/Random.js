f14529f17f40f9d75ada4bd31a36accdb2ece8b5
xgqfrms <xgqfrms@outlook.com>
2018-10-09 00:02:48 +0800

    components

---
+++ src/components/Random.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Random extends React.Component {
+    render() {
+        // First, some logic that must happen before rendering:
+        const n = Math.floor(Math.random() * 10 + 1);
+        // Next, a return statement using that logic:
+        return <h1>The number is {n}!</h1>;
+    }
+}
+
+ReactDOM.render(
+    <Random />,
+    document.getElementById("app")
+);
